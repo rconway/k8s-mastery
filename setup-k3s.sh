6e15c293fe386ab819876db14ca0f684e971d9d4
Richard Conway <richard@rconway.co.uk>
2020-05-25 17:55:06 +0100

    Additions for dockercoin + dashboard.

--- setup-k3s.sh
+++ setup-k3s.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Install k3s
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--disable servicelb" sh -s -
+curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --disable servicelb
 
 # Install kubectl
 #
