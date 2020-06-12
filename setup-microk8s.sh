224c8a0830a1f489989c3b7675a9f69ed9108064
Richard Conway <richard@rconway.co.uk>
2020-06-12 16:10:29 +0100

    setup-microk8s script commented out the parts that are informational

--- setup-microk8s.sh
+++ setup-microk8s.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install snap
-sudo apt install snapd
+# sudo apt install snapd
 
 # Install microk8s
 #
@@ -9,34 +9,37 @@ sudo apt install snapd
 sudo snap install microk8s --classic
 #
 # Specific version
-sudo snap install microk8s --channel=1.17/stable --classic
+# sudo snap install microk8s --channel=1.17/stable --classic
 
 # Add current user to the group
+# -> probably have to logout/in to take effect
 sudo usermod -a -G microk8s $USER
 
 # kubectl
 #
 # Use the microk8s version (via alias for convenience)
-alias kubectl='microk8s.kubectl'
+# alias kubectl='microk8s.kubectl'
 # Install latest version
-curl -Lo $HOME/.local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl \
-  && chmod +x $HOME/.local/bin/kubectl
+# curl -Lo $HOME/.local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl && chmod +x $HOME/.local/bin/kubectl
 # Install specific version - v1.17
-sudo snap install kubectl --channel=1.17/stable --classic
+# sudo snap install kubectl --channel=1.17/stable --classic
 
 # kubectl config
 microk8s config >${HOME}/.kube/config
 
 # kubectl bash completion
-echo "source <(kubectl completion bash)" >>$HOME/.bash_completion
+# echo "source <(kubectl completion bash)" >>$HOME/.bash_completion
 
 # core extensions
-microk8s enable dns dashboard ingress
+microk8s enable dns ingress
 
 # LoadBalancer - prompts for more info
 microk8s enable metallb
 
+# kube config
+microk8s config >$HOME/.kube/config
+
 # shpod
-kubectl apply -f https://k8smastery.com/shpod.yaml
-kubectl attach --namespace=shpod -ti shpod
-kubectl delete -f https://k8smastery.com/shpod.yaml
+# kubectl apply -f https://k8smastery.com/shpod.yaml
+# kubectl attach --namespace=shpod -ti shpod
+# kubectl delete -f https://k8smastery.com/shpod.yaml
